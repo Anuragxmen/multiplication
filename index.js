4f4b4d8565726787b936e0c9b76b115451d63e73
Anuragxmen <121851676+Anuragxmen@users.noreply.github.com>
2023-04-09 09:15:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const num1 = Math.ceil(Math.random()*20);
+const num2 = Math.ceil(Math.random()*10);
+const quesEl = document.getElementById("question");
+const inputEl = document.getElementById("input");
+const formEl = document.getElementById("form");
+const scoreEl = document.getElementById("score")
+let score = JSON.parse(localStorage.getItem("score"));
+if(!score){
+    score = 0;
+}
+scoreEl.innerText = `score:${score}`
+quesEl.innerText=`What is ${num1} multiply by ${num2} ?`;
+const correctAns = num1 * num2 ;
+formEl.addEventListener("submit",()=>{
+    const userAnsw = +inputEl.value
+    if(userAnsw===correctAns){
+        score++;
+        updateLocalStorage()
+    }else{
+        score--;
+        updateLocalStorage()
+    }
+})
+
+function updateLocalStorage(){
+    localStorage.setItem("score", JSON.stringify(score))
+}
